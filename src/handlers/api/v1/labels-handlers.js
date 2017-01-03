16a91a318c822aba22d55d987a102434fb77cfe5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-03 12:04:29 +0900

    Create api mock

---
+++ src/handlers/api/v1/labels-handlers.js
@@ -0,0 +1,45 @@
+export function labelsIndexHandler(req, res) {
+  const now = new Date();
+
+  res.json([{
+    id: 1,
+    name: 'TODAY',
+    order: 0,
+    createdAt: now,
+    updateAt: now,
+  }, {
+    id: 2,
+    name: 'LATER',
+    order: 0,
+    createdAt: now,
+    updateAt: now,
+  }]);
+}
+
+export function labelsCreateHandler(req, res) {
+  const now = new Date();
+
+  res.json({
+    id: 1,
+    name: 'TODAY',
+    order: 0,
+    createdAt: now,
+    updateAt: now,
+  });
+}
+
+export function labelsUpdateHandler(req, res) {
+  const now = new Date();
+
+  res.json({
+    id: 1,
+    name: 'UPDATED TODAY',
+    order: 0,
+    createdAt: now,
+    updateAt: now,
+  });
+}
+
+export function labelsDeleteHandler(req, res) {
+  res.json({id: 1});
+}
---
+++ src/handlers/api/v1/tasks-handlers.js
@@ -0,0 +1,65 @@
+export function tasksIndexHandler(req, res) {
+  const now = new Date();
+
+  res.json([{
+    id: 1,
+    content: 'task',
+    completed: false,
+    createdAt: now,
+    updateAt: now,
+    order: 0,
+    label: {
+      id: 0,
+      name: 'TODAY',
+    },
+  }, {
+    id: 2,
+    content: 'task',
+    completed: false,
+    createdAt: now,
+    updateAt: now,
+    order: 0,
+    label: {
+      id: 0,
+      name: 'TODAY',
+    },
+  }]);
+}
+
+export function tasksCreateHandler(req, res) {
+  const now = new Date();
+
+  res.json({
+    id: 1,
+    content: 'task',
+    completed: false,
+    createdAt: now,
+    updateAt: now,
+    order: 0,
+    label: {
+      id: 0,
+      name: 'TODAY',
+    },
+  });
+}
+
+export function tasksUpdateHandler(req, res) {
+  const now = new Date();
+
+  res.json({
+    id: 1,
+    content: 'updated task',
+    completed: false,
+    createdAt: now,
+    updateAt: now,
+    order: 0,
+    label: {
+      id: 0,
+      name: 'TODAY',
+    },
+  });
+}
+
+export function tasksDeleteHandler(req, res) {
+  res.json({id: 1});
+}
