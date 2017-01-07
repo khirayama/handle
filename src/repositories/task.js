b7d51c03284594c74fcd1733d7221c977ad33389
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-07 12:46:31 +0900

    Fix hostname

--- src/repositories/task.js
+++ src/repositories/task.js
@@ -2,7 +2,7 @@ import {Collection} from 'libs/micro-repository';
 
 class Task extends Collection {
   constructor() {
-    const hostname = process.env.HOSTNAME || 'http://0.0.0.0:3000';
+    const hostname = (process.env.NODE_ENV === 'production') ? process.env.HOSTNAME : 'http://0.0.0.0:3000';
     super(`${hostname}/api/v1/tasks`);
   }
 }
