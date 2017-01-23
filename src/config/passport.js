55621b83aed0622c47df54139d39cad078651e49
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-24 08:17:02 +0900

    Create initial labels and tasks

--- src/config/passport.js
+++ src/config/passport.js
@@ -4,7 +4,7 @@ import {Strategy as InstagramStrategy} from 'passport-instagram';
 
 import getLocalAddress from 'libs/get-local-address';
 
-import {User} from 'models';
+import {User, Task, Label} from 'models';
 
 const {ipv4} = getLocalAddress();
 const hostname = (process.env.NODE_ENV === 'production') ? process.env.HOSTNAME : `http://${ipv4[0].address}:3000`;
@@ -40,8 +40,59 @@ passport.use(new TwitterStrategy(config.twitter,
       uid: profile.id,
       username: profile.username,
       imageUrl: profile.photos[0].value,
-    }}).spread(user => {
-      done(null, user);
+    }}).spread((user, created) => {
+      if (created) {
+        Label.bulkCreate([{
+          userId: user.id,
+          name: 'TODAY',
+          priority: 0,
+          visibled: true,
+        }, {
+          userId: user.id,
+          name: 'LATER',
+          priority: 1,
+          visibled: true,
+        }, {
+          userId: user.id,
+          name: 'SCHEDULE',
+          priority: 2,
+          visibled: true,
+        }]).then(() => {
+          Label.findOne({
+            where: {userId: user.id, priority: 0}
+          }).then(label => {
+            Task.bulkCreate([{
+              userId: user.id,
+              labelId: label.id,
+              content: 'Swipe right to complete!',
+              priority: 0,
+              completed: false,
+            }, {
+              userId: user.id,
+              labelId: label.id,
+              content: 'Swipe left to delete!',
+              priority: 1,
+              completed: false,
+            }, {
+              userId: user.id,
+              labelId: label.id,
+              content: 'Hold task to sort!',
+              priority: 2,
+              completed: false,
+            }, {
+              userId: user.id,
+              labelId: label.id,
+              content: '↓ click "Add task"!',
+              priority: 3,
+              completed: false,
+            }]).then(() => {
+              done(null, user);
+            });
+          });
+        });
+      } else {
+        done(null, user);
+      }
     });
   }
 ));
