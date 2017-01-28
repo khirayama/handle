6eb0fc357ebad81db53c4afa263a826d46883aa0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-28 18:58:35 +0900

    Update collecton

--- src/libs/micro-repository/collection.js
+++ src/libs/micro-repository/collection.js
@@ -39,6 +39,7 @@ export class Collection {
           const item = this._cache.response[index];
           if (item.id === id) {
             resolve(item);
+            return;
           }
         }
         this._find(id, config, resolve, reject);
@@ -56,27 +57,65 @@ export class Collection {
   // command
   create(data, config) {
     return new Promise((resolve, reject) => {
-      this._request.post(this._url(), data, config).then(({data}) => {
-        this._clear();
-        resolve(data);
+      this._request.post(this._url(), data, config).then(res => {
+        resolve(res.data);
+        setTimeout(() => {
+          if (this._cache.response !== null) {
+            this._cache.response.push(res.data);
+          }
+        }, 0);
       }).catch(error => reject(error));
     });
+    // this._clear();
+    // return new Promise((resolve, reject) => {
+    //   this._request.post(this._url(), data, config).then(res => {
+    //     resolve(res.data);
+    //   }).catch(error => reject(error));
+    // });
   }
   update(data, config) {
     return new Promise((resolve, reject) => {
-      this._request.put(this._url(data.id), data, config).then(({data}) => {
-        this._clear();
-        resolve(data);
+      this.find(data.id, config).then(item => {
+        Object.assign(item, data);
+        this._request.put(this._url(data.id), data, config).then(res => {
+          Object.assign(item, res.data);
+          resolve(res.data)
+        });
       }).catch(error => reject(error));
     });
+    // this._clear();
+    // return new Promise((resolve, reject) => {
+    //   this._request.put(this._url(data.id), data, config).then(({data}) => {
+    //     resolve(data);
+    //   }).catch(error => reject(error));
+    // });
   }
   delete(id, config) {
     return new Promise((resolve, reject) => {
-      this._request.delete(this._url(id), config).then(({data}) => {
-        this._clear();
-        resolve(data);
-      }).catch(error => reject(error));
+      if (this._cache.response !== null) {
+        this._cache.response = this._cache.response.filter(res => {
+          return res.id !== id;
+        });
+        this._request.delete(this._url(id), config).then(res => {
+          resolve(res.data);
+          setTimeout(() => {
+            this._cache.response = this._cache.response.filter(res_ => {
+              return res_.id !== id;
+            });
+          }, 0);
+        }).catch(error => reject(error));
+      } else {
+        this._request.delete(this._url(id), config).then(res => {
+          resolve(res.data);
+        }).catch(error => reject(error));
+      }
     });
+    // this._clear();
+    // return new Promise((resolve, reject) => {
+    //   this._request.delete(this._url(id), config).then(({data}) => {
+    //     resolve(data);
+    //   }).catch(error => reject(error));
+    // });
   }
 
   // for cache
