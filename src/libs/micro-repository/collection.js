c61628aea1c5bc7b26ab2f8e71869466b3a814a4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-28 19:03:13 +0900

    Revert micro repository

--- src/libs/micro-repository/collection.js
+++ src/libs/micro-repository/collection.js
@@ -56,66 +56,66 @@ export class Collection {
   }
   // command
   create(data, config) {
-    return new Promise((resolve, reject) => {
-      this._request.post(this._url(), data, config).then(res => {
-        resolve(res.data);
-        setTimeout(() => {
-          if (this._cache.response !== null) {
-            this._cache.response.push(res.data);
-          }
-        }, 0);
-      }).catch(error => reject(error));
-    });
-    // this._clear();
     // return new Promise((resolve, reject) => {
     //   this._request.post(this._url(), data, config).then(res => {
     //     resolve(res.data);
+    //     setTimeout(() => {
+    //       if (this._cache.response !== null) {
+    //         this._cache.response.push(res.data);
+    //       }
+    //     }, 0);
     //   }).catch(error => reject(error));
     // });
-  }
-  update(data, config) {
+    this._clear();
     return new Promise((resolve, reject) => {
-      this.find(data.id, config).then(item => {
-        Object.assign(item, data);
-        this._request.put(this._url(data.id), data, config).then(res => {
-          Object.assign(item, res.data);
-          resolve(res.data)
-        });
+      this._request.post(this._url(), data, config).then(res => {
+        resolve(res.data);
       }).catch(error => reject(error));
     });
-    // this._clear();
+  }
+  update(data, config) {
     // return new Promise((resolve, reject) => {
-    //   this._request.put(this._url(data.id), data, config).then(({data}) => {
-    //     resolve(data);
+    //   this.find(data.id, config).then(item => {
+    //     Object.assign(item, data);
+    //     this._request.put(this._url(data.id), data, config).then(res => {
+    //       Object.assign(item, res.data);
+    //       resolve(res.data)
+    //     });
     //   }).catch(error => reject(error));
     // });
+    this._clear();
+    return new Promise((resolve, reject) => {
+      this._request.put(this._url(data.id), data, config).then(({data}) => {
+        resolve(data);
+      }).catch(error => reject(error));
+    });
   }
   delete(id, config) {
-    return new Promise((resolve, reject) => {
-      if (this._cache.response !== null) {
-        this._cache.response = this._cache.response.filter(res => {
-          return res.id !== id;
-        });
-        this._request.delete(this._url(id), config).then(res => {
-          resolve(res.data);
-          setTimeout(() => {
-            this._cache.response = this._cache.response.filter(res_ => {
-              return res_.id !== id;
-            });
-          }, 0);
-        }).catch(error => reject(error));
-      } else {
-        this._request.delete(this._url(id), config).then(res => {
-          resolve(res.data);
-        }).catch(error => reject(error));
-      }
-    });
-    // this._clear();
     // return new Promise((resolve, reject) => {
-    //   this._request.delete(this._url(id), config).then(({data}) => {
-    //     resolve(data);
-    //   }).catch(error => reject(error));
+    //   if (this._cache.response !== null) {
+    //     this._cache.response = this._cache.response.filter(res => {
+    //       return res.id !== id;
+    //     });
+    //     this._request.delete(this._url(id), config).then(res => {
+    //       resolve(res.data);
+    //       setTimeout(() => {
+    //         this._cache.response = this._cache.response.filter(res_ => {
+    //           return res_.id !== id;
+    //         });
+    //       }, 0);
+    //     }).catch(error => reject(error));
+    //   } else {
+    //     this._request.delete(this._url(id), config).then(res => {
+    //       resolve(res.data);
+    //     }).catch(error => reject(error));
+    //   }
     // });
+    this._clear();
+    return new Promise((resolve, reject) => {
+      this._request.delete(this._url(id), config).then(({data}) => {
+        resolve(data);
+      }).catch(error => reject(error));
+    });
   }
 
   // for cache
