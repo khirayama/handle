e69b31bed046420fb6a1193019d91a95cdf44434
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-02-02 23:47:45 +0900

    Change config.json to config.js

---
+++ src/config/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'handle_database_development',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    migrationStorage: 'json',
+    migrationStoragePath: 'sequelize-meta.json',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'handle_database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    migrationStorage: 'json',
+    migrationStoragePath: 'sequelize-meta.json',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    dialect: 'mysql',
+    dialectOptions: {
+      socketPath: '/cloudsql/' + process.env.MYSQL_SOCKETPATH,
+    },
+    migrationStorage: 'json',
+    migrationStoragePath: 'sequelize-meta.json',
+  }
+}
