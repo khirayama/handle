70bd9432f2c3b0d1ed91adcd08e80360fabd901f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-02-04 20:58:48 +0900

    Update deploy npm script

--- create-app-yaml.js
+++ create-app-yaml.js
@@ -1,6 +1,44 @@
 const fs = require('fs');
-fs.writeFile('app.yaml', `runtime: nodejs
+fs.writeFile('app.yaml', `
+
+runtime: nodejs
+
 env: flex
+
+threadsafe: true
+
+skip_files:
+  - ^(.*/)?.*/node_modules/.*$
+
+resources:
+  cpu: 0.2
+  memory_gb: 0.5
+  disk_size_gb: 10
+
+manual_scaling:
+  instances: 1
+
+handlers:
+- url: /bundle.js
+  static_files: dist/public/bundle.js
+  upload: dist/public/bundle.js
+
+- url: /index.css
+  static_files: dist/public/index.css
+  upload: dist/public/index.css
+
+- url: /fonts/materialicons.woff2
+  static_files: dist/public/fonts/materialicons.woff2
+  upload: dist/public/fonts/materialicons.woff2
+
+- url: /fonts/fonts/NotoSansJP-Bold.woff2
+  static_files: dist/public/fonts/fonts/NotoSansJP-Bold.woff2
+  upload: dist/public/fonts/fonts/NotoSansJP-Bold.woff2
+
+- url: /fonts/fonts/NotoSansJP-Regular.woff2
+  static_files: dist/public/fonts/fonts/NotoSansJP-Regular.woff2
+  upload: dist/public/fonts/fonts/NotoSansJP-Regular.woff2
+
 env_variables:
   APPLICATION_URL: '${process.env.APPLICATION_URL}'
 
@@ -16,6 +54,7 @@ env_variables:
 
 beta_settings:
   cloud_sql_instances: '${process.env.CLOUD_SQL_INSTANCES}'
+
 `, (err) => {
   if (err) {
     console.log(err);
