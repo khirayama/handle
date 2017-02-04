0a134aff9f207ac84cb2209651b38b3821265201
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-02-04 21:02:24 +0900

    Fix initialize

--- src/handlers/application-handlers.js
+++ src/handlers/application-handlers.js
@@ -62,53 +62,96 @@ export function applicationHandler(req, res) {
   data.dispatch = store.dispatch.bind(store);
   data.query = req.query;
 
-  Task.fetch(data.config).then(tasks => {
-    Label.fetch(data.config).then(labels => {
-      store.dispatch({
-        type: actionTypes.SET_INITIAL_VALUE,
-        tasks,
-        labels,
-      });
-      router.initialize(pathname, data).then(() => {
-        const state = store.getState();
+  if (req.isAuthenticated()) {
+    Task.fetch(data.config).then(tasks => {
+      Label.fetch(data.config).then(labels => {
+        store.dispatch({
+          type: actionTypes.SET_INITIAL_VALUE,
+          tasks,
+          labels,
+        });
+        router.initialize(pathname, data).then(() => {
+          const state = store.getState();
 
-        const head = router.getHead(req.path);
-        const content = ReactDOM.renderToString(
-          <Connector
-          router={router}
-          path={req.path}
-          store={store}
+          const head = router.getHead(req.path);
+          const content = ReactDOM.renderToString(
+            <Connector
+            router={router}
+            path={req.path}
+            store={store}
 
-          transitionName="page-transition"
-          transitionEnterTimeout={PAGE_TRANSITION_TIME}
-          transitionLeaveTimeout={PAGE_TRANSITION_TIME}
-          />
-        );
+            transitionName="page-transition"
+            transitionEnterTimeout={PAGE_TRANSITION_TIME}
+            transitionLeaveTimeout={PAGE_TRANSITION_TIME}
+            />
+          );
 
-        res.send(`
-          <!DOCTYPE html>
-          <html lang="${state.lang}">
-            <head>
-              <meta charset="utf-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-              <title>${head.title}</title>
+          res.send(`
+            <!DOCTYPE html>
+            <html lang="${state.lang}">
+              <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+                <title>${head.title}</title>
 
-              <!-- standalone for android-->
-              <link rel="manifest" href="/manifest.json">
-              <!-- standalone for ios-->
-              <meta name="apple-mobile-web-app-capable" content="yes">
+                <!-- standalone for android-->
+                <link rel="manifest" href="/manifest.json">
+                <!-- standalone for ios-->
+                <meta name="apple-mobile-web-app-capable" content="yes">
 
-              <link rel="stylesheet" href="/index.css">
-              <script src="/bundle.js" defer></script>
-            </head>
-            <body>
-              <section class="application ${state.ui}">${content}</section>
-            </body>
-            <script>var state = ${JSON.stringify(state)}</script>
-          </html>
-        `
-        );
+                <link rel="stylesheet" href="/index.css">
+                <script src="/bundle.js" defer></script>
+              </head>
+              <body>
+                <section class="application ${state.ui}">${content}</section>
+              </body>
+              <script>var state = ${JSON.stringify(state)}</script>
+            </html>
+          `
+          );
+        }).catch(error => console.log(error));
       }).catch(error => console.log(error));
     }).catch(error => console.log(error));
-  }).catch(error => console.log(error));
+  } else {
+    router.initialize(pathname, data).then(() => {
+      const state = store.getState();
+
+      const head = router.getHead(req.path);
+      const content = ReactDOM.renderToString(
+        <Connector
+        router={router}
+        path={req.path}
+        store={store}
+
+        transitionName="page-transition"
+        transitionEnterTimeout={PAGE_TRANSITION_TIME}
+        transitionLeaveTimeout={PAGE_TRANSITION_TIME}
+        />
+      );
+
+      res.send(`
+        <!DOCTYPE html>
+        <html lang="${state.lang}">
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+            <title>${head.title}</title>
+
+            <!-- standalone for android-->
+            <link rel="manifest" href="/manifest.json">
+            <!-- standalone for ios-->
+            <meta name="apple-mobile-web-app-capable" content="yes">
+
+            <link rel="stylesheet" href="/index.css">
+            <script src="/bundle.js" defer></script>
+          </head>
+          <body>
+            <section class="application ${state.ui}">${content}</section>
+          </body>
+          <script>var state = ${JSON.stringify(state)}</script>
+        </html>
+      `
+      );
+    }).catch(error => console.log(error));
+  }
 }
