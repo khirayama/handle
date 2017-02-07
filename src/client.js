ffb03af44c3af723fe8fba77071ae757b247eed9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-02-08 08:38:56 +0900

    [BETA] Add polling function

--- src/client.js
+++ src/client.js
@@ -9,7 +9,29 @@ import routes from 'config/routes';
 
 import reducer from 'reducers';
 
+import actionTypes from 'constants/action-types';
+
+import Task from 'repositories/task';
+import Label from 'repositories/label';
+
 const PAGE_TRANSITION_TIME = 600;
+
+function polling(dispatch) {
+  setInterval(() => {
+    const result = document.hasFocus();
+    if (result) {
+      Task.fetch().then(tasks => {
+        Label.fetch().then(labels => {
+          dispatch({
+            type: actionTypes.SET_INITIAL_VALUE,
+            tasks,
+            labels,
+          });
+        });
+      });
+    }
+  }, 10000);
+}
 
 window.addEventListener('contextmenu', event => {
   event.preventDefault();
@@ -21,6 +43,8 @@ window.addEventListener('DOMContentLoaded', () => {
   const store = createStore(window.state, reducer);
 
   const router = new Router(routes);
+
+  polling(store.dispatch.bind(store));
 
   ReactDOM.render(
     <Connector
