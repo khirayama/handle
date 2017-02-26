151f51f9d80d6a73424b87de150e6229640baa98
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-02-27 06:52:59 +0900

    Update icons

--- src/handlers/application-handlers.js
+++ src/handlers/application-handlers.js
@@ -27,10 +27,10 @@ function template(head, state, content) {
 
         <!-- standalone for android-->
         <link rel="manifest" href="/manifest.json">
-        <link rel="icon" sizes="192x192" href="/images/icon192.png">
+        <link rel="icon" sizes="192x192" href="/images/icon@4.png">
         <!-- standalone for ios-->
         <meta name="apple-mobile-web-app-capable" content="yes">
-        <link rel="apple-touch-icon" sizes="76x76" href="/images/icon72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/images/icon@1.5.png">
 
         <link rel="stylesheet" href="/index.css">
         <script src="/bundle.js" defer></script>
